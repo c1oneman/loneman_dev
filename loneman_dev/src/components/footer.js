55365d12024ab65e0a1e8c3b53de3b75d9a21cd8
BuildTools <clayloneman@gmail.com>
2021-02-01 13:22:28 -0600

    update margins

--- loneman_dev/src/components/footer.js
+++ loneman_dev/src/components/footer.js
@@ -14,10 +14,12 @@ const Footer = (props) => {
                 <a href='https://twitter.com/c1oneman' target='_blank' rel="noopener noreferrer"><i className={`fab fa-twitter fa-1x`} /></a>
                 <a href='https://www.linkedin.com/in/clayton-loneman-7788771b7/' target='_blank' rel="noopener noreferrer"><i className="fab fa-linkedin fa-1x" /></a>
                 <a href='https://www.github.com/c1oneman' target='_blank' rel="noopener noreferrer"><i className="fab fa-github fa-1x" /></a>
-
             </div>
-            <RetroHitCounter
-                slug={'dev'} className='m1' />
+            <div className='m1' >
+                <RetroHitCounter
+                    slug={'dev'} />
+            </div>
+
             <a href='https://netlify.com' target='_blank' rel="noopener noreferrer">
                 <div className='netlify shadow-4'>
                     <img className='netlifyLogo' src='/images/full-logo-light.png' alt='Deployed with Netlify' />
