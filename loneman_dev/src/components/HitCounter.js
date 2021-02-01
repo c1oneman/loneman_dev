bdd2b823f7e505b003c388359f535e946244f9bc
BuildTools <clayloneman@gmail.com>
2021-02-01 12:42:57 -0600

    update for cors build success

--- loneman_dev/src/components/HitCounter.js
+++ loneman_dev/src/components/HitCounter.js
@@ -9,20 +9,19 @@ function HitCounter({ slug }) {
     }
     // Invoke the function by making a request.
     // Update the URL to match the format of your platform.
-    fetch(`https://loneman.dev/.netlify/functions/register-hit?slug=${slug}`),
-      {
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Headers": "Content-Type",
-          "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
-        },
+    fetch(`https://loneman.dev/.netlify/functions/register-hit?slug=${slug}`, {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
       }
-        .then((res) => res.json())
-        .then((json) => {
-          if (typeof json.hits === "number") {
-            setHits(json.hits);
-          }
-        });
+    })
+      .then((res) => res.json())
+      .then((json) => {
+        if (typeof json.hits === "number") {
+          setHits(json.hits);
+        }
+      });
   }, [slug]);
   if (typeof hits === "undefined") {
     return null;
