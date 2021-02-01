dbb59a4716dbe831c268c8fe560f104488f5861f
BuildTools <clayloneman@gmail.com>
2021-02-01 12:09:37 -0600

    added counter

--- loneman_dev/src/App.js
+++ loneman_dev/src/App.js
@@ -6,33 +6,35 @@ import Portfolio from './components/portfolio'
 import Footer from './components/footer'
 import Skills from './components/skills'
 import Contact from './components/Contact'
-
+import RetroHitCounter from './components/HitCounter'
 import { Route, Switch } from 'react-router-dom'
 
 function App() {
   return (
     <div className='App'>
-      <div className = 'content-wrap'>
-      <Nav />
-      <Switch>
-        <Route path='/contact'>
-          <Contact/>
-        </Route>
-        <Route path='/projects'>
-          <Portfolio />
-        </Route>
-        <Route path='/skillset'>
-          <Skills />
-        </Route>
-        <Route path='/'>
-          <Header />
-          <Portfolio />
-          <Skills />
-        </Route>
-      </Switch>
+      <div className='content-wrap'>
+        <Nav />
+        <Switch>
+          <Route path='/contact'>
+            <Contact />
+          </Route>
+          <Route path='/projects'>
+            <Portfolio />
+          </Route>
+          <Route path='/skillset'>
+            <Skills />
+          </Route>
+          <Route path='/'>
+            <Header />
+            <Portfolio />
+            <Skills />
+            <RetroHitCounter
+              slug={'homepage'} />
+          </Route>
+        </Switch>
       </div>
 
-    <Footer/>
+      <Footer />
     </div>
   );
 }
