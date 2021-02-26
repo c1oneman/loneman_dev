22ada6295039caa959bdd18f38f70c04551aebe7
BuildTools <clayloneman@gmail.com>
2021-02-26 13:36:40 -0600

    change resume, unused vars

--- loneman_dev/src/components/Resume.js
+++ loneman_dev/src/components/Resume.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 
 import "../App.css";
 import { Document, Page, pdfjs } from "react-pdf";
@@ -6,16 +6,10 @@ import { ContentView } from "../styled-components/structure";
 function Resume() {
   pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
-  const [numPages, setNumPages] = useState(null);
-  function onDocumentLoadSuccess({ numPages }) {
-    setNumPages(numPages);
-  }
   return (
     <ContentView>
       <Document
-        file="/documents/Clayton Loneman Resume.pdf"
-        onLoadSuccess={onDocumentLoadSuccess}
-      >
+        file="/documents/Clayton Loneman Resume.pdf"      >
         <Page pageNumber={1} />
       </Document>
     </ContentView>
