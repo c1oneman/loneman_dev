bd7af7d3e1522c2beff4c190009f15b56029fe6c
BuildTools <clayloneman@gmail.com>
2021-05-21 14:47:51 -0500

    remove logos, fix netlify logo to be reactive

--- loneman_dev/src/index.js
+++ loneman_dev/src/index.js
@@ -1,11 +1,12 @@
+import "react-app-polyfill/ie11";
+import "react-app-polyfill/stable";
 import React from "react";
 import { render } from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter as Router } from "react-router-dom";
-import "react-app-polyfill/ie11";
-import "react-app-polyfill/stable";
+
 render(
   // Wrap the <App /> in a provider
   <Router>
